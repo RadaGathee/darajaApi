65510ede8797764e8282c01b030c3506c9d2f94d
RadaGathee <vitayatanks001@gmail.com>
2024-07-13 15:03:10 +0300

    To host it

--- stk_initiate.php
+++ stk_initiate.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://-------------herokuapp.com/callback_url.php';   #To work: Host it Live, i.e on Heroku
+  $CallBackURL = 'https://-------------herokuapp.com/callback_url.php';   #To work: Host it Live, i.e on Heroku.
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
